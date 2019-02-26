5f3eba866c18b13f74ef331fa8d26f02677741b4
aowsenek <aowsenek@gmail.com>
2019-02-26 09:55:42 -0700

    Does not compile (yet)

---
+++ RockPaperScissors.sol
@@ -0,0 +1,111 @@
+pragma solidity ^0.5.0;
+
+contract RockPaperScissors {
+/* Given a choice (e.g. "rock", "paper" or "scissors")
+and a random/blinding string, returns a commitment
+of the pair. Note calls to this function occur offline
+(i.e. do not appear on the blockchain).
+If you leave the'pure'
+keyword in the function signature,
+function calls to this won't publish to the blockchain.*/
+address p1;
+address p2;
+bytes32 hashchoice1;
+bytes32 hashchoice2;
+string public choice1;
+string public choice2;
+address winner;
+bool resolved;
+uint wager;
+
+function encode_commitment(string memory choice, string memory rand) public pure returns (bytes32) { 
+    return sha256(sha256(choice)^sha256(rand));
+
+}
+/* Accepts a commitment (generated via encode_commitment)
+and a wager of ethereum*/
+function play(bytes32 commitment) public payable { 
+    if(p1 == 0){
+        p1 = msg.sender;
+        hashchoice1 = commitment;
+        wager = msg.value;
+    }
+    else{
+        p2 = msg.sender;
+        hashchoice2 = commitment;
+        if(msg.value < wager){
+            require(msg.value >= wager);
+        }
+        else if(msg.value > wager){
+            uint excess = msg.value - wager;
+            p2.send(excess); //probably won't work
+        }
+    }
+}
+/* Once both players have commited (called play()),
+they reveal their choice and blinding string.
+This function verifies the commitment is correct
+and after both players submit, determines the winner.
+*/
+function reveal(string memory choice, string memory rand) public { 
+    if(msg.sender == p1){
+        assert(hashchoice1 == sha256(sha256(choice)^sha256(rand)));
+        choice1 = choice;
+    }
+    else{
+        assert(hashchoice2 == sha256(sha256(choice)^sha256(rand)));
+        choice2 = choice;
+    }
+    if(choice1 != 0 && choice2 != 0){
+        if(sha256(choice1) == sha256("rock")){
+            if(sha256(choice2) ==  sha256("rock")){
+                winner = 0;
+            }
+            else if(sha256(choice2) ==  sha256("paper")){
+                winner = p2;
+            }
+            else if(sha256(choice2) ==  sha256("scissors")){
+                winner = p1;
+            }
+        }
+        else if(sha256(choice1) == sha256("paper")){
+            if(sha256(choice2) ==  sha256("rock")){
+                winner = p1;
+            }
+            else if(sha256(choice2) ==  sha256("paper")){
+                winner = 0;
+            }
+            else if(sha256(choice2) ==  sha256("scissors")){
+                winner = p2;
+            }
+        }
+        else if(sha256(choice1) == sha256("scissors")){
+            if(sha256(choice2) ==  sha256("rock")){
+                winner = p2;
+            }
+            else if(sha256(choice2) ==  sha256("paper")){
+                winner = p1;
+            }
+            else if(sha256(choice2) ==  sha256("scissors")){
+                winner = 0;
+            }
+        }
+
+        resolved = true;
+    }
+}
+/* After both players reveal, this allows the winner
+to claim their reward (both wagers).
+In the event of a tie, this function should let
+each player withdraw their initial wager.
+*/
+function withdraw() public { 
+    if(winner = 0 && resolved == true){
+        p1.send(wager);
+        p2.send(wager);
+    }
+    else if(resolved == true){
+        winner.send(wager*2);
+    }
+}
+}
